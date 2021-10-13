33284de5072519f9ce071a1bc9789e5a5d8e4dc2
张子辰 <zichen350@gmail.com>
2021-10-13 22:25:58 +0800

    修改Windows下system("pause")的Linux等价物的实现（system是在sh中执行的，而read是bash命令）.

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ int main([[maybe_unused]]int argc,[[maybe_unused]]char **argv)
 		event_queue.call_and_pop();
 		//call()函数可能有输出，所以在call之后才输出用于分割的空行
 		if(printed)std::cout<<"\n";
-		if(printed)system(R"(read -s -n 1 -p "请按任意键继续..." && echo "")");
+		if(printed)system(R"(bash -c "read -s -n 1 -p \"请按任意键继续...\" && echo \"\"")");
 	}
 	return 0;
 }
