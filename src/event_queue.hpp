60580295d37f946adbe560c4b6bf342d01a37049
张子辰 <zichen350@gmail.com>
2021-10-13 13:00:32 +0800

    引入事件的签名，尝试解决上一个commit中的问题.

--- src/event_queue.hpp
+++ src/event_queue.hpp
@@ -14,12 +14,16 @@ class event_t
 {
 public:
 	event_t(uint64_t time,const std::string &name);
-	virtual void call()const=0;						//完成event需要进行的操作
-	virtual bool print(std::ostream &)const;		//输出操作的情况, 返回是否真的输出了内容
+	virtual void call()const=0;					//完成event需要进行的操作
+	virtual bool print(std::ostream &)const;	//输出操作的情况, 返回是否真的输出了内容
 	virtual ~event_t()=default;
-	uint64_t time;									//事件发生的时间
-	uint64_t order;									//事件被创建的顺序（用于确定同一时刻的多个事件的处理顺序）
-	std::string name;								//事件名称
+	uint64_t time;								//事件发生的时间
+	uint64_t order;								//事件被创建的顺序（用于确定同一时刻的多个事件的处理顺序）
+	std::string name;							//事件名称
+	//事件签名。对于持续性事件，有时需要知道事件是否“正在发生”，为此引入事件签名。
+	//当事件开始发生时，事件签名被放入一个集合中，同时事件被放入事件队列，在调用call后，事件被从该集合中删去
+	//在这个集合中的事件是正在发生的事件
+	std::string signature;
 protected:
 	std::ostream& output_time(std::ostream &)const;	//以[hh:mm:ss]的格式输出时间
 private:
--- src/variables.hpp
+++ src/variables.hpp
@@ -3,15 +3,17 @@
 //This file is part of the 电梯模拟器.
 #include "event_queue.hpp"
 #include <array>
+#include <unordered_set>
 #include "lift.hpp"
 #include "passenger.hpp"
 #include "constants.hpp"
 
 namespace variable
 {
-extern event_queue_t event_queue;		//事件队列
+extern event_queue_t event_queue;		                //事件队列
 //在每层楼的电梯口的排队的人
 extern std::array<zzc::queue<passenger_t>,constant::max_floor-constant::min_floor+1> waiting_queues_up,waiting_queues_down;
-extern wbutton_t wall_buttons;			//每层楼墙上的按钮
-extern std::array<lift_t,2> lifts;		//两部电梯
+extern wbutton_t wall_buttons;			                //每层楼墙上的按钮
+extern std::array<lift_t,2> lifts;		                //两部电梯
+extern std::unordered_set<std::string> event_happening; //正在发生的事件
 }
